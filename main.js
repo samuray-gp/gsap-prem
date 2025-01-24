fb79c5532b391322cd24656963a36fdb611ad448
Денис Гурков <denisgurkov@MacBook-Pro-Denis.local>
2025-01-24 08:58:01 +0700

    main

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-// const firstName = "Denis"
-// firstName = "Elena"
-// console.log("Hello", firstName)
-let firstName = "Denis"
-firstName = "Elena"
-console.log("Hello", firstName);
+const age = 27;
+console.log(typeof age);
+const bigInt =26354785899n;
+console.log(typeof bigInt);
+const firstName ="Denis";
+console.log(typeof firstName);
+let age2;
+console.log(age2);
+const obj = {
+    name: "Denis",
+    age: 27,
+    isWorked: true
+}
+
+const num = 10
+const num2 = 3
+console.log(num + num2);
+console.log(num % num2);
+
